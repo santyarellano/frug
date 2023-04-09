23b4f09d7a77d315dc79999d1432da4322080db2
santyarellano <santy.arellano039@gmail.com>
2023-04-08 20:05:09 -0600

    refactor: use index buffer instead of vertex

--- src/lib.rs
+++ src/lib.rs
@@ -55,9 +55,17 @@ impl Vertex {
 // - - - - - TEST! - - - - -
 // We should remove this in the future so we can create these in frug usage.
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] },
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4
 ];
 // - - - - - TEST! - - - - -
 
@@ -73,7 +81,8 @@ pub struct FrugInstance {
     background_color: wgpu::Color,
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
-    num_vertices: u32
+    index_buffer: wgpu::Buffer,
+    num_indices: u32
 }
 
 /// Implementation of FrugInstance methods
@@ -178,7 +187,13 @@ impl FrugInstance {
             usage: wgpu::BufferUsages::VERTEX
         });
 
-        let num_vertices = VERTICES.len() as u32;
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX
+        });
+
+        let num_indices = INDICES.len() as u32;
 
         Self {
             window,
@@ -190,7 +205,8 @@ impl FrugInstance {
             background_color,
             render_pipeline,
             vertex_buffer,
-            num_vertices
+            index_buffer,
+            num_indices
         }
     }
 
@@ -230,7 +246,8 @@ impl FrugInstance {
 
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_vertices, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
