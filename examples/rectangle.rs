a105cdcd300158ad6d0a6e6b66459be48d92dd98
santyarellano <santy.arellano039@gmail.com>
2023-04-14 16:17:59 -0600

    setup wip texture

--- examples/rectangle.rs
+++ examples/rectangle.rs
@@ -4,11 +4,15 @@ extern crate frug;
 
 fn main() {
 
+    let mut x = 0.0;
+
     let update_loop  = move |instance: &mut FrugInstance| {
 
         instance.clear_staging_buffers_data();
-        instance.add_rectangle(0.0, 0.0, 0.5, 0.5, [0.0, 0.5, 0.5]);
+        instance.add_rectangle(x, 0.0, 0.5, 0.5, [0.0, 0.5, 0.5]);
         instance.update_buffers_with_staging();
+
+        x += 0.001;
     };
     
     frug::run("My Window", update_loop);
