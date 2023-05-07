86cfc598b37abbf7d4123372f070b82320c765a1
santyarellano <santy.arellano039@gmail.com>
2023-05-06 21:09:20 -0600

    guide camera and input

--- examples/guide.rs
+++ examples/guide.rs
@@ -6,11 +6,19 @@ fn main() {
     let img_bytes = include_bytes!("frog.png");
     let frog_text_idx = frug_instance.load_texture(img_bytes);
 
-    let update_function = move |instance: &mut frug::FrugInstance, _input: &frug::InputHelper| {
+    let update_function = move |instance: &mut frug::FrugInstance, input: &frug::InputHelper| {
+        // Checking our input
+        if input.key_held(frug::VirtualKeyCode::Right) {
+            instance.camera.eye.x -= 0.01;
+            instance.camera.target.x -= 0.01;
+        } else if input.key_held(frug::VirtualKeyCode::Left) {
+            instance.camera.eye.x += 0.01;
+            instance.camera.target.x += 0.01;
+        }
+
+        // Rendering
         instance.clear();
-
         instance.add_tex_rect(-0.25, 0.0, 0.5, 0.5, frog_text_idx);
-
         instance.update_buffers();
     };
 
