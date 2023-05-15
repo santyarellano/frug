65e5bdd861135177c7ef69a3594e7f0590abe417
santyarellano <santy.arellano039@gmail.com>
2023-05-15 15:23:17 -0600

    updated readme

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Welcome to the FRUG repo!
+
+In here you will find the source code for the FRUG library.
+
+## About
+
+FRUG is intended to provide a similar abstraction layer over graphics programming as to how SDL does for C++, meaning that it should provide developers enough control and flexibility to implement their own architectures & design patterns, yet simplifying the process of working with graphics so developers won't have to worry about implementing all the repetitive tasks related to getting things to the screen.
+
+Please check out [the documentation](https://santyarellano.github.io/frug_book/) to learn more about the project and how to use it!
+
+> On a side note, please take into account that the project is in it's first iteration and many features are still under development or missing. Feel free to leave your comments and evaluation in the [following survey](https://forms.gle/m2h7v2wkjwFTpUrP7)!
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,6 @@
 //! FRUG is intended to provide a similar abstraction layer over graphics programming as to how SDL does for C++, meaning that it should provide developers enough control and flexibility to implement their own architectures & design patterns, yet simplifying the process of working with graphics so developers won't have to worry about implementing all the repetitive tasks related to getting things to the screen.
 //!
-//! FRUG aims to include the following features (unchecked items are the ones still under development):
-//! - [x] Window management
-//! - [x]  Loading & rendering textures
-//! - [ ]  Rotating textures
-//! - [x]  Scaling textures
-//! - [ ]  Alpha blending for textures
-//! - [ ]  Choosing a specific backend (aka. Direct X, Metal, Vulkan, etc.)
-//! - [ ]  Writing and using custom shaders
-//! - [ ]  Handle window state events
-//! - [x]  Handle Mouse input
-//! - [x]  Handle Keyboard input
-//! - [ ]  Playing audio
-//! - [ ]  Configure audio
+//! Please see [the documentation](https://santyarellano.github.io/frug_book/) for more information.
 
 use cgmath::Matrix4;
 // Exported libs
