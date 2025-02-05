43776da6942b247df2ea001a230654c23bb2d11f
santyarellano <santy.arellano039@gmail.com>
2025-02-05 11:57:43 -0800

    switch to sdl3

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! FRUG is intended to provide a similar abstraction layer over graphics programming as to how SDL does for C++, meaning that it should provide developers enough control and flexibility to implement their own architectures & design patterns, yet simplifying the process of working with graphics so developers won't have to worry about implementing all the repetitive tasks related to getting things to the screen.
 //!
 //! Please see [the documentation](https://santyarellano.github.io/frug_book/) for more information.
-
+//!
 //! I'M MIGRATING THIS WHOLE THING FROM WGPU TO SDL3. PLEASE HOLD ON!!!
