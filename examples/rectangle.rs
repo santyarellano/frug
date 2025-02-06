1b936eb50c1fa6bf269a90cb27428271d8f7dec5
santyarellano <santy.arellano039@gmail.com>
2025-02-06 10:52:28 -0800

    fn create window works

--- examples/rectangle.rs
+++ examples/rectangle.rs
@@ -1,5 +1,34 @@
-use frug;
+use std::time::Duration;
+
+use frug::{self, Color, Event, Keycode};
 
 fn main() {
-    frug::test();
+    let graphics_context = sdl3::init().unwrap();
+    let mut canvas = frug::create_window(&graphics_context);
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = graphics_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,57 +4,19 @@
 //!
 //! I'M MIGRATING THIS WHOLE THING FROM WGPU TO SDL3. PLEASE HOLD ON!!!
 
-use sdl3::event::Event;
-use sdl3::keyboard::Keycode;
-use sdl3::pixels::Color;
+pub use sdl3::event::Event;
+pub use sdl3::keyboard::Keycode;
+pub use sdl3::pixels::Color;
+
 use sdl3::rect::Rect;
 use sdl3::render::Canvas;
 use sdl3::video::Window;
-use std::time::Duration;
+use sdl3::Sdl;
 
-//pub use sdl3::pixels::Color;
-
-pub fn test() {
-    let sdl_context = sdl3::init().unwrap();
+/// Returns a canvas where we can draw.
+pub fn create_window(sdl_context: &Sdl) -> Canvas<Window> {
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem
-        .window("rust-sdl3 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas();
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
-    'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-        // The rest of the game loop goes here...
-
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }
-}
-
-pub fn create_window() -> Canvas<Window> {
-    let sdl_context = sdl3::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
         .window("FRUG Window", 800, 600)
         .position_centered()
