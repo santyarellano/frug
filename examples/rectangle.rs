1e099c89e2037dd347a850eb0ea3fb0beb4ad542
santyarellano <santy.arellano039@gmail.com>
2025-02-05 17:03:00 -0800

    got window

--- examples/rectangle.rs
+++ examples/rectangle.rs
@@ -1,19 +1,5 @@
-use frug::FrugInstance;
-
-extern crate frug;
+use frug;
 
 fn main() {
-    let (frug_instance, event_loop) = frug::new("My Window");
-
-    let mut x = 0.0;
-
-    let update_function = move |instance: &mut FrugInstance, _input: &frug::InputHelper| {
-        instance.clear();
-        instance.add_colored_rect(x, 0.0, 0.5, 0.5, [0.0, 0.5, 0.5]);
-        instance.update_buffers();
-
-        x += 0.001;
-    };
-
-    frug_instance.run(event_loop, update_function);
+    frug::test();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,80 @@
 //! Please see [the documentation](https://santyarellano.github.io/frug_book/) for more information.
 //!
 //! I'M MIGRATING THIS WHOLE THING FROM WGPU TO SDL3. PLEASE HOLD ON!!!
+
+use sdl3::event::Event;
+use sdl3::keyboard::Keycode;
+use sdl3::pixels::Color;
+use sdl3::rect::Rect;
+use sdl3::render::Canvas;
+use sdl3::video::Window;
+use std::time::Duration;
+
+//pub use sdl3::pixels::Color;
+
+pub fn test() {
+    let sdl_context = sdl3::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("rust-sdl3 demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+
+pub fn create_window() -> Canvas<Window> {
+    let sdl_context = sdl3::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("FRUG Window", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let canvas = window.into_canvas();
+    return canvas;
+}
+
+pub fn draw_rectangles(canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    let _ = canvas.fill_rect(Rect::new(50, 50, 200, 150));
+
+    canvas.set_draw_color(Color::RGB(0, 255, 0));
+    let _ = canvas.fill_rect(Rect::new(300, 50, 200, 150));
+
+    canvas.set_draw_color(Color::RGB(0, 0, 255));
+    let _ = canvas.fill_rect(Rect::new(550, 50, 200, 150));
+
+    canvas.present();
+}
